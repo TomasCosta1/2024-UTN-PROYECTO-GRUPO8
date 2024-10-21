7a8ab2fe7a498931a9ca968d9c4d734e01de2fcb
naimguar <naimguar@gmail.com>
2024-10-21 13:32:52 -0300

    Pagina, estilos y componente creados del Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AdminPage from './pages/AdminPage';
 import ProductPage from './pages/ProductPage';
+import LoginPage from './pages/LoginPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -12,6 +13,7 @@ const App = () => {
       <div>
         <Header />
         <Routes>
+          <Route path="/login" element={<LoginPage />} />  /*tengo que sacarle el footer y el Header*/
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/product/:id" element={<ProductPage />} />
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/Login.css";
+
+
+
+const Login = () => {
+    return (
+        <section>
+            <img src="../../favicon.ico" alt="logo" />
+            <h1>Nombre del restaurant</h1>
+        </section>
+    );
+}
+
+export default Login;
